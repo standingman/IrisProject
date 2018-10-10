9779039adfa9b89bcfa1ff9b681dcaa608b99cba
zhangzhihao <standingman@yeah.net>
2018-10-10 11:54:41 +0800

    gorm设置

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"IrisProject/routes"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/middleware/logger"
 	"github.com/kataras/iris/middleware/recover"
 )
+
 
 func newApp() *iris.Application {
 	app := iris.New()
@@ -19,6 +22,10 @@ func newApp() *iris.Application {
 }
 
 func main() {
+	db, _ := gorm.Open("mysql", "root:mysql.com2017@tcp(106.14.96.72:3306)/common_db?charset=utf8&parseTime=True&loc=Local")
+	db.DB().SetMaxIdleConns(10)
+	db.DB().SetMaxOpenConns(100)
+	defer db.Close()
 	app := newApp()
 	app.Run(iris.Addr(":8080"))
 }
